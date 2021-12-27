f242583b84a12c21c3a88981a219c048e46ed276
enddrop28 <mea.enddrop@gmail.com>
2021-12-27 12:53:49 +0000

    [Finish]post-view

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,6 +3,7 @@ class HomesController < ApplicationController
   def top
     # @categories = Category.all
     @post = Post.limit(4).order(" created_at DESC ")
+    @posts = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(4).pluck(:post_id))
   end
   
   def about
--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -3,11 +3,11 @@ class PostCommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @book = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     @post_comment = current_user.post_comments.new(post_comment_params)
     @post_comment.post_id = @post.id
     if @post_comment.save
-    redirect_to posts_path
+    redirect_to post_path(@post.id)
     else
      render 'posts/show'
     end
